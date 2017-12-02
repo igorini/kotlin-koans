04966f1b435dda3f4979d55fb48cabdd5c9c4b05
Igor Maguskin <maguskin.igor@gmail.com>
2017-12-02 04:57:48 +0000

    Implement tasks 14-27

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,18 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val yearCompare = year.compareTo(other.year)
+        if (yearCompare != 0) return yearCompare
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+        val monthCompare = month.compareTo(other.month)
+        if (monthCompare != 0) return monthCompare
+
+        return dayOfMonth.compareTo(other.dayOfMonth)
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +20,6 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(date: MyDate) = date >= start && date <= endInclusive
+}
